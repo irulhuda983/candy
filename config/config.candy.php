12c738335c5c69b951cd14ea47edba2a7d79cb8f
irulhuda <irulhuda983@gmail.com>
2021-01-13 08:37:34 +0700

    kekurangan

---
+++ config/config.candy.php
@@ -0,0 +1,8 @@
+<?php
+$copyright = 'SMK AK CBT';
+
+define("VERSI", "2.7.0");
+define("REVISI", "0");
+define("APLIKASI", "SMK AK CBT");
+define("NAMA_DATABASE", "cbt");
+define("BASEPATH", "cbt.smkalkyai.sch.id"); 
